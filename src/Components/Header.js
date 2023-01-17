1ef8b633af4075184b4e9767220b032c4f7d3f92
harun tahid <haruntahid3660@gmail.com>
2023-01-18 03:14:20 +0600

    tahas edit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,100 +20,100 @@ const drawerWidth = 240;
 const navItems = ["How it works", "Platforms", "Fees", "About"];
 
 function DrawerAppBar(props) {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen((prevState) => !prevState);
-    };
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
 
-    const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-            <Box sx={{ my: 1 }}>
-                <img src="/img/logo.png" alt="logo" />
-            </Box>
-            <Divider />
-            <List>
-                {navItems.map((item) => (
-                    <ListItem key={item} disablePadding>
-                        <ListItemButton sx={{ textAlign: "center" }}>
-                            <ListItemText primary={item} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Box sx={{ my: 1 }}>
+        <img style={{ height: "27px" }} src="/img/logo.png" alt="logo" />
+      </Box>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
-    const container =
-        window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
-    return (
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <CssBaseline />
-            <AppBar
-                component="nav"
-                sx={{
-                    background: "#000",
-                }}
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{
+          background: "#000",
+        }}
+      >
+        <Container>
+          {" "}
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleOpenNavMenu}
+              sx={{ mr: 2, display: { md: "none" } }}
             >
-                <Container>
-                    {" "}
-                    <Toolbar>
-                        <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            edge="start"
-                            onClick={handleOpenNavMenu}
-                            sx={{ mr: 2, display: { md: "none" } }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
+              <MenuIcon />
+            </IconButton>
 
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            {navItems.map((data) => {
-                                return (
-                                    <MenuItem onClick={handleCloseNavMenu}>
-                                        <Typography>{data}</Typography>
-                                    </MenuItem>
-                                );
-                            })}
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {navItems.map((data) => {
+                return (
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography>{data}</Typography>
+                  </MenuItem>
+                );
+              })}
 
-                            {/* {pages.map((page) => (
+              {/* {pages.map((page) => (
                                 <MenuItem
                                     key={page}
                                     onClick={handleCloseNavMenu}
@@ -123,8 +123,8 @@ function DrawerAppBar(props) {
                                     </Typography>
                                 </MenuItem>
                             ))} */}
-                        </Menu>
-                        {/* <Typography
+            </Menu>
+            {/* <Typography
               sx={{
                 flexGrow: 1,
                 display: { xs: "inline" },
@@ -135,76 +135,81 @@ function DrawerAppBar(props) {
             >
               VALV X
             </Typography> */}
-                        <Box
-                            sx={{
-                                flexGrow: 1,
-                                display: { xs: "flex" },
-                                fontWeight: "bold",
-                                fontSize: "19px",
-                                cursor: "pointer",
-                                alignItems: "center",
-                            }}
-                        >
-                            <img src="/img/logo.png" alt="logo" />
-                        </Box>
-                        <Box sx={{ display: { xs: "none", md: "block" } }}>
-                            {navItems.map((item) => (
-                                <Button key={item} sx={{ color: "#fff" }}>
-                                    {item}
-                                </Button>
-                            ))}
-                        </Box>
-                        <Button
-                            sx={{
-                                background:
-                                    "linear-gradient(94.32deg, #007AFF 1.74%, #A9148B 96.61%)",
-                                borderRadius: "10px",
-                                width: "134px",
-                                height: "40px",
-                                color: "#fff",
-                                fontSize: "14px",
-                                ml: 2,
-                                fontWeight: "400",
-                            }}
-                        >
-                            Go to app
-                        </Button>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-            <Box component="nav">
-                <Drawer
-                    container={container}
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: { xs: "block", md: "none" },
-                        "& .MuiDrawer-paper": {
-                            boxSizing: "border-box",
-                            width: drawerWidth,
-                        },
-                    }}
-                >
-                    {drawer}
-                </Drawer>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "flex" },
+                fontWeight: "bold",
+                fontSize: "19px",
+                cursor: "pointer",
+                alignItems: "center",
+              }}
+            >
+              <img style={{ height: "27px" }} src="/img/logo.png" alt="logo" />
+              <Typography
+                sx={{ fontSize: "26px", marginLeft: "10px", fontWeight: "600" }}
+              >
+                VALV.FI
+              </Typography>
             </Box>
-            <Box component="main" sx={{ p: 3 }}>
-                <Toolbar />
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+              {navItems.map((item) => (
+                <Button key={item} sx={{ color: "#fff" }}>
+                  {item}
+                </Button>
+              ))}
             </Box>
-        </Box>
-    );
+            <Button
+              sx={{
+                background:
+                  "linear-gradient(94.32deg, #007AFF 1.74%, #A9148B 96.61%)",
+                borderRadius: "10px",
+                width: "134px",
+                height: "40px",
+                color: "#fff",
+                fontSize: "14px",
+                ml: 2,
+                fontWeight: "400",
+              }}
+            >
+              Go to app
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", md: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
+  );
 }
 
 DrawerAppBar.propTypes = {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window: PropTypes.func,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
 export default DrawerAppBar;
